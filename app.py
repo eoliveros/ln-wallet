f43fdf722cc8413273cd3906a62359188e00372d
Erick Oliveros <eoliveros@redratclothing.co.nz>
2021-10-13 20:49:12 +1300

    allow payment_form to take bitcoin payments and send to address.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, Markup
 from flask_cors import CORS
 
 import utils
 from new_address import new_address
+from bitcoin_script import sending_bitcoin
 
 app = Flask(__name__)
 CORS(app)
+app.secret_key = 'this is a rand0m str1ng'
 
 @app.route('/')
 def index():
@@ -27,8 +29,17 @@ def bitcoind_getinfo_ep():
 def lightningd_getinfo_ep():
     return 'not implemented'
 
-@app.route('/payment_form')
+@app.route('/payment_form', methods=['GET', 'POST'])
 def payment_form_ep():
+    if request.method == 'POST':
+        address = request.form['address']
+        amount = request.form['amount']
+        message = request.form['message']
+        try:
+            txid = utils.bitcoind_rpc().sendtoaddress(address, amount)
+            flash(Markup(f'<a href="https://testnet.bitcoinexplorer.org/tx/{txid}">Transaction ID: {txid}</a>'), 'success')
+        except Exception as e:
+            flash(Markup(e.args[0]['message']), "danger")
     return render_template("payment_form.html")
 
 @app.route('/new_address')
