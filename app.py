aff19ee7c763287fefaf96062b6713a189cc0f88
Daniel Newton <djpnewton@gmail.com>
2021-10-13 19:19:35 +1300

    fix app bitcoind rpc connection when running outside of docker

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template, request, redirect
 from flask_cors import CORS
+
+import utils
 from new_address import new_address
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def index():
+    return '<a href="/bitcoind_getnetworkinfo">bitcoind getnetworkinfo</a><br><a href="/bitcoind_getwalletinfo">bitcoind getwalletinfo</a><br><a href="/lightningd_getinfo">lightningd getinfo</a>'
+
+@app.route('/bitcoind_getnetworkinfo')
+def bitcoind_getnetworkinfo_ep():
+    return str(utils.bitcoind_rpc().getnetworkinfo())
+
+@app.route('/bitcoind_getwalletinfo')
+def bitcoind_getwalletinfo_ep():
+    return str(utils.bitcoind_rpc().getwalletinfo())
+
+@app.route('/bitcoind_getwalletinfo')
+def bitcoind_getinfo_ep():
+    return utils.bitcoind_rpc().getwalletinfo()
+
+@app.route('/lightningd_getinfo')
+def lightningd_getinfo_ep():
+    return 'not implemented'
 
 @app.route('/payment_form')
-def payment_form():
+def payment_form_ep():
     return render_template("payment_form.html")
 
 @app.route('/new_address')
--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+import os
+
 import bitcointx.rpc
 
 def bitcoind_rpc():
+    if 'SERVICE_URL' in os.environ:
+        return bitcointx.rpc.RPCCaller(service_url=os.environ['SERVICE_URL'])
     return bitcointx.rpc.RPCCaller(conf_file='bitcoin.conf')
