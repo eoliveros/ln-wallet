21b6eaa7b187c2d8dc5208cef680fd34428ce10e
Alex <aboyle@zap.me>
2021-10-18 10:41:24 +1300

    Remove comments in ln.py

--- ln.py
+++ ln.py
@@ -19,8 +19,6 @@ class LightningInstance():
     def create_invoice(self, amount, msg):
         return self.instance.invoice(amount, "lbl{}".format(random.random()), msg)
     def send_invoice(self, bolt11):
-        # so far just returns a decoded bolt11
         decoded_bolt11 = self.instance.decodepay(bolt11)
         route = self.instance.getroute(decoded_bolt11["routes"][0][0]["pubkey"], decoded_bolt11["msatoshi"], 1)
         return self.instance.sendpay(route["route"], decoded_bolt11["payment_hash"])
-        #return decoded_bolt11
