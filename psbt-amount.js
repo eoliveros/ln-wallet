d5a5562bc42864499c9ea75b5679474cea7cc7b0
Alex <aboyle@zap.me>
2022-01-09 15:51:40 +1300

    Get decode PSBT amount working

--- psbt-amount.js
+++ psbt-amount.js
@@ -2,18 +2,19 @@ const decodePsbt = require('psbt');
 const txHexDecoder = require("raw-transaction-hex-decoder");
 const dpsbt = decodePsbt;
 
-const getUnsignedPSBTAmount = function(psbtString) {
-	const buffer = Buffer.from(psbtString, 'base64');
-	const bufString = buffer.toString('hex');
-	const rawTx = dpsbt.decodePsbt({psbt: bufString})["unsigned_transaction"];
-	const btcDecodedRawTx = txHexDecoder.decodeRawUTXO(rawTx);
-	var txOutputs = btcDecodedRawTx["outs"];
-	// remove change address amount from outputs
-	var changeOutput = txOutputs.pop();
-	var totalSent = 0;
-	for(var i=0; i < txOutputs.length; i++) {
-		totalSent+=txOutputs[i]["value"];
-	};
-	return `${totalSent/100000000}`;
+module.exports = {
+	getUnsignedPSBTAmount : function(psbtString) {
+		const buffer = Buffer.from(psbtString, 'base64');
+		const bufString = buffer.toString('hex');
+		const rawTx = dpsbt.decodePsbt({psbt: bufString})["unsigned_transaction"];
+		const btcDecodedRawTx = txHexDecoder.decodeRawUTXO(rawTx);
+		var txOutputs = btcDecodedRawTx["outs"];
+		// remove change address amount from outputs
+		var changeOutput = txOutputs.pop();
+		var totalSent = 0;
+		for(var i=0; i < txOutputs.length; i++) {
+			totalSent+=txOutputs[i]["value"];
+		};
+		return `${totalSent/100000000}`;
+	},
 };
-
